f17c1bd4e457d218cc7b124645bb362a31b6ad15
AlfonsoQuiroz <aquirozh_24@yahoo.es>
2023-06-09 23:01:53 -0500

    desplegable

--- js/index.js
+++ js/index.js
@@ -75,8 +75,12 @@
 
 'use strict';
 
+import faq from "./modules/faq.js";
 import header from "./modules/header.js";
+import slider from "./modules/slider.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   header();
+  slider();
+  faq();
 });
